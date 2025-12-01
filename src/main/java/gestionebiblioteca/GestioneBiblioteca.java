1d1ada8ce9afdb451a41b5c879c7bd198912cae3
Giuseppe <Giuseppe@AX16PRO.homenet.telecomitalia.it>
2025-12-01 20:58:24 +0100

    Aggiunte le classi del model e creati View (FXML) e Controller JavaFX

---
+++ src/main/java/gestionebiblioteca/GestioneBiblioteca.java
@@ -0,0 +1,40 @@
+package gestionebiblioteca;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Giuseppe
+ */
+public class GestioneBiblioteca extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Loader recupera la struttura grafica dal nostro file FXML
+        Parent root = FXMLLoader.load(getClass().getResource("GestioneBibliotecaView.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Gestione Biblioteca");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
