72636f5452d4e0a9226726f4e4af74b2f2cb0323
Loren <Loren@192.168.1.18>
2025-12-03 15:50:24 +0100

    Aggiunto scheletro e documentazione di ValidaDati e Utente

--- src/main/java/gestionebiblioteca/Utente.java
+++ src/main/java/gestionebiblioteca/Utente.java
@@ -5,10 +5,79 @@
  */
 package gestionebiblioteca;
 
+import java.util.List;
+
 /**
  *
  * @author Giuseppe
  */
 public class Utente {
+    private String nome;
+    private String cognome;
+    private String matricola;
+    private String email;
+    private List<Prestito> prestitiAttivi;
     
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public String getCognome(){
+        return this.cognome;
+    }
+    
+    public String getMatricola(){
+        return this.matricola;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public List<Prestito> getPrestitiAttivi(){
+        return this.prestitiAttivi;
+    }
+    
+    /**
+     * @brief Aggiungi il prestito.
+     * 
+     * Tale metodo consente di aggiungere un prestito all'elenco di libri prestati 
+     * all'utente.
+     * 
+     * @param[in] prestito Il prestito da aggiungere.
+     * 
+     * @pre L'utente non ha raggiunto il limite di prestiti.
+     * @post Il prestito viene aggiunto all'elenco prestiti dell'utente.
+     * 
+     * @see DF-1
+     * @see verificaLimitePrestito(Utente utente).
+    */
+    public void aggiungiPrestito(Prestito prestito){
+        
+    }
+    
+    /**
+     * @brief Rimuovi il prestito.
+     * 
+     * Tale metodo consente di rimuovere un prestito dall'elenco di libri prestati 
+     * all'utente.
+     * 
+     * @param[in] prestito Il prestito da rimuovere.
+     * 
+     * @pre Il prestito è presente nell'elenco prestiti dell'utente.
+     * @post Il prestito viene rimosso dall'elenco prestiti dell'utente.
+     * 
+     *
+     * @see IF-8
+    */
+    public void rimuoviPrestito(Prestito prestito){
+        
+    }
+    
+    @Override
+    public String toString(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
+    
--- src/main/java/gestionebiblioteca/ValidaDati.java
+++ src/main/java/gestionebiblioteca/ValidaDati.java
@@ -9,6 +9,19 @@ package gestionebiblioteca;
  *
  * @author Giuseppe
  */
-public interface ValidaDati {
+
+public interface ValidaDati<T> {
+    
+    /**
+     * @brief Verifica che l'oggetto sia valido.
+     * 
+     * Il metodo consente di stabilire se l'oggetto rispetta i criteri per
+     * essere registrato.
+     * 
+     * @param[in] o L'oggetto di cui valutare la validità. 
+     * 
+     * @return 'true' se l'oggetto soddisfa i requisiti, altrimenti 'false'.
+    */
+    public boolean isValido(T object);
     
 }
