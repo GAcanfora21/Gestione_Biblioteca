50fae5fa532fba3010781212af6bf02334e74419
acrisci05 <a.crisci14@studenti.unisa.it>
2025-12-05 14:28:41 +0100

    Aggiunto scheletro e documentazione di Libro e GestioneFile

--- src/main/java/biblioteca/main/GestioneFile.java
+++ src/main/java/biblioteca/main/GestioneFile.java
@@ -5,10 +5,75 @@
  */
 package biblioteca.main;
 
+import java.util.List;
+
+
 /**
- *
- * @author Giuseppe
+ * @file GestioneFile.java
+ * @brief Questa classe gestisce il salvataggio e il caricamento dei dati su file CSV
+ * 
+ * Questa classe gestisce il caricamento e il salvataggio degli elenchi dei libri, utenti e prestiti
+ * su file in formato CSV
+ * 
+ * @see DF-4
+ * @see BF-4
+ * @see FC-7
+ * @author Antonio
+ * @date December 04, 2025
+ * @version 1.0
  */
 public class GestioneFile {
+    
+    
+    /**
+     * @brief Costruttore di GestioneFile.
+     * 
+     * @post libri != null && libri.isEmpty()
+     * @post utenti != null && utenti.isEmpty()
+     * @post prestiti != null && prestiti.isEmpty()
+     */
+    public GestioneFile(){
+        
+    }
+    
+    
+    /**
+     * @brief questo metodo salva i dati della Biblioteca su file CSV
+     * 
+     * @param[in] nomeFile Percorso del file CSV.
+     * @param[in] libri Lista dei libri da salvare.
+     * @param[in] utenti Lista degli utenti da salvare.
+     * @param[in] prestiti Lista dei prestiti da salvare.
+     * 
+     * @pre nomeFile ha come estensione ".csv"
+     * @pre nomeFile != null
+     * @post I dati vengono scritti nel file. Se esiste, viene sovrascritto.
+     * 
+     * @see IF-15
+     * @see FC-7
+     */
+    public static void salvaFileCSV(String nomeFile, List<Libro> libri, List<Utente> utenti, List<Prestito> prestiti){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    /**
+     * @brief questo metodo carica i dati della biblioteca da file CSV.
+     * 
+     * @param[in] nomeFile Percorso del file CSV.
+     * @param[out] libri Lista in cui caricare i libri.
+     * @param[out] utenti Lista in cui caricare gli utenti.
+     * @param[out] prestiti Lista in cui caricare i prestiti.
+     * 
+     * @pre nomeFile ha come estensione ".csv"
+     * @pre nomeFile != null
+     * @post le liste vengono riempite con i dati passati da parametro in ingresso nomeFile
+     * 
+     * @return Oggetto GestioneFile contente i dati passati da parametro 
+     * @see IF-16
+     * @see FC-7
+     */
+    public static void caricaFileCSV(String nomeFile, List<Libro> libri, List<Utente> utenti, List<Prestito> prestiti){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
--- src/main/java/biblioteca/main/Libro.java
+++ src/main/java/biblioteca/main/Libro.java
@@ -6,11 +6,107 @@
 package biblioteca.main;
 
 /**
- *
- * @author Giuseppe
+ * @file Libro.java
+ * @brief Definisce attributi e metodi della classe Libro
+ * 
+ * @invariant titolo != null
+ * @invariant autori != null
+ * @invariant annoDiPubblicazione > 0
+ * @invariant codiceIdentificativo != null && codiceIdentificativo.length() == 10
+ * @invariant numCopie >= 0
+ * 
+ * 
+ * @see IF-1
+ * @see IF-2
+ * @see DF-2
+ * @author Antonio
+ * @date December 04, 2025
+ * @version 1.0
  */
 public class Libro {
     
+    private String titolo;
+    private String autori;
+    private int annoDiPubblicazione;
+    private String codiceIdentificativo;
+    private int numCopie;
     
+    /**
+     * @brief costruttore di Libro
+     * 
+     * @param[in] titolo Titolo del libro.
+     * @param[in] autori Autori del libro.
+     * @param[in] annoDiPubblicazione Anno di pubblicazione.
+     * @param[in] codiceIdentificativo Codice univoco (10 cifre).
+     * @param[in] numCopie Numero di copie disponibili.
+     */
+    public Libro(String titolo, String autori, int annoDiPubblicazione, String codiceIdentificativo, int numCopie){
+        //
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getAutori() {
+        return autori;
+    }
+
+    public int getAnnoDiPubblicazione() {
+        return annoDiPubblicazione;
+    }
+
+    public String getCodiceIdentificativo() {
+        return codiceIdentificativo;
+    }
+
+    public int getNumCopie() {
+        return numCopie;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setAutori(String autori) {
+        this.autori = autori;
+    }
+
+    public void setAnnoDiPubblicazione(int annoDiPubblicazione) {
+        this.annoDiPubblicazione = annoDiPubblicazione;
+    }
+
+    public void setCodiceIdentificativo(String codiceIdentificativo) {
+        this.codiceIdentificativo = codiceIdentificativo;
+    }
+
+    public void setNumCopie(int numCopie) {
+        this.numCopie = numCopie;
+    }
+    
+
+    /**
+     * @brief Incrementa il numero di copie dell'istanza di un libro
+     * 
+     * @post libro.numCopie = libro.numCopie + 1
+     */
+    public void aggiungiCopie(){
+        
+    }
+    
+    /**
+     * @brief decrementa di 1 il numero di copie dell'istanza di un libro
+     * 
+     * @post libro.numCopie = libro.numCopie - 1
+     */
+    public void rimuoviCopie(){
+        
+    }
+    
+    
+    @Override
+    public String toString(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
