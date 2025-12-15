6b72cc701be20154d41792c503de839823ad637c
Giuseppe <Giuseppe@AX16PRO.homenet.telecomitalia.it>
2025-12-15 10:52:23 +0100

    Aggiunta documentazione nella classe MessaggiDiControllo.

--- src/main/java/biblioteca/validatori/MessaggiDiControllo.java
+++ src/main/java/biblioteca/validatori/MessaggiDiControllo.java
@@ -8,11 +8,22 @@ package biblioteca.validatori;
 import javafx.scene.control.Alert;
 
 /**
- *
+ * @brief Classe che permette all'utente di visualizzare un messaggio
+ * 
+ * La classe permette di creare un pop-up di avviso, errore o informazione all'interno dell'applicazione JavaFX.
+ * 
  * @author Giuseppe
+ * @date December 15, 2025
+ * @version 1.0
  */
 public class MessaggiDiControllo {
     
+        /**
+         * @brief Mostra una finestra di dialogo a schermo
+         * 
+         * @param[in] type Il tipo di avviso (es. AllertType.ERROR, mostra un pop-up di errore)
+         * @param[in] messaggio Il messaggio da visualizzare nella finestra di dialogo 
+         */
         public static void mostraAlert(Alert.AlertType type, String messaggio) {
             Alert alert = new Alert(type);
             alert.setContentText(messaggio);
